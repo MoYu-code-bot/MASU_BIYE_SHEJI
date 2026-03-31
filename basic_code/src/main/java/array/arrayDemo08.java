4f55904f264cd226065879d72abcc3257d2fa6be
江磊 <3077127059@qq.com>
2026-03-31 17:35:03 +0800

    更新

--- basic_code/src/main/java/array/arrayDemo08.java
+++ basic_code/src/main/java/array/arrayDemo08.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Scanner;
+
 public class arrayDemo08 {
     public static void main(String[] args) {
       /*  1、给定一个整数数组nums和一个整数目标值target,
@@ -7,8 +9,7 @@ public class arrayDemo08 {
       要求1：只要输出第一对满足要求的情况
       要求2：输出所有满足要求的情况
         */
-        /*
-        int[] arr = {10, 20, 30, 40, 50};
+        /*int[] arr = {10, 20, 30, 40, 50};
 
         int target = 50;
         //要求1：只要输出第一对满足要求
@@ -38,15 +39,14 @@ public class arrayDemo08 {
                     break;
                 }
             }
-        }
-        */
+        }*/
 
 
 
         /*2、给出两个有序数组arr1 和 arr2 ,将两个数组中的数据合并到一个大数组中
 
         */
-        /*int[] arr1 = {1, 3, 5, 7, 9};
+       /* int[] arr1 = {1, 3, 5, 7, 9};
         int[] arr2 = {2, 4, 6, 8, 10};
 
         int sum = arr1.length + arr2.length;
@@ -86,37 +86,46 @@ public class arrayDemo08 {
 
 
 
-
         /*3、给定一个递增的有序数组和一个目标值，在数组中找到目标值，并打印其索引
         如果目标值不存在于数组中，打印应插入的位置,并将目标值插入数组中,打印数组信息
         */
+        Scanner sc = new Scanner(System.in);
 
         int[] arr = {1, 2, 4, 6, 8, 9, 10};
-        int target = 5;
+        System.out.println("请输入目标值：");
+        int target = sc.nextInt();
 
         int m = 0;
         for (int i = 0; i < arr.length; i++) {
             if(target == arr[i]){
                 System.out.println("数组中索引为：" + i);
+                m = i;
                 break;
 
             }
             if(target < arr[i]){
+                m = i;
                 System.out.println("数组中插入的位置为：" + i);
                 break;
             }
         }
+        if(m == 0){
+            System.out.println("数组中插入的位置为：" + arr.length);
 
-/*        while (m < arr.length){
-            if (target > arr[m]){
-                m++;
-            }else {
-                target = arr[m];
-                System.out.println(arr[m]);
-                break;
-            }
-        }*/
-
+        }
+        int[] newArr = new int[arr.length + 1];
+        for (int i = 0; i < m; i++) {
+            newArr[i] = arr[i];
+        }
+        newArr[m] = target;
+        for (int i = m; i < arr.length; i++) {
+            newArr[i + 1] = arr[i];
+        }
+        System.out.println("插入后的数组为：");
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print(newArr[i] + " ");
+        }
+        System.out.println();
 
 
 

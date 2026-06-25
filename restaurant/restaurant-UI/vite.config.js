22834a8bed235c1c20fe492f3e3cd3e20afe1291
江磊 <3077127059@qq.com>
2026-06-25 14:17:39 +0800

    # FixedBUG

--- restaurant/restaurant-UI/vite.config.js
+++ restaurant/restaurant-UI/vite.config.js
@@ -12,11 +12,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8088',
+        target: 'http://localhost:8103',
         changeOrigin: true
       },
       '/admin': {
-        target: 'http://localhost:8088',
+        target: 'http://localhost:8103',
         changeOrigin: true
       }
     }
